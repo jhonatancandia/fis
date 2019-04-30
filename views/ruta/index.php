7450bd3ca285685cface7039c7b55c1c7a752ec9
Karen Avendaño <kar371603@gmail.com>
2019-04-30 21:25:53 +0100

    modificado personal

--- views/ruta/index.php
+++ views/ruta/index.php
@@ -22,7 +22,7 @@
                     <a class="nav-link" href="#"></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="personal">PERSONAL</a>
+                    <a class="nav-link" href="../vacaciones/personal">PERSONAL</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="../vacaciones/cargos">CARGOS</a>
