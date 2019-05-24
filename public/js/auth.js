b6cfd7284914a1f2499530b7c35cc04285e0e9ee
jhonatancandia <tar-jhonatan@hotmail.com>
2019-05-24 10:11:46 -0400

    Modificado los enlaces al archivo auth, solo tiene la vista registrar

--- public/js/auth.js
+++ public/js/auth.js
@@ -4,38 +4,4 @@ function auth(){
         auth();
 }
 
-$(document).ready(function () {
-    $("#username").blur(function () { 
-        if($("#username").val() == ""){
-            $("#alert-login").html('<div class="alert alert-warning alert-dismissible fade show" role="alert"><span>Debe ingresar su nombre de usuario</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-        }
-    });
-    $("#password").blur(function () { 
-        if($("#password").val() == ""){
-            $("#alert-login").html('<div class="alert alert-warning alert-dismissible fade show" role="alert"><span>Debe ingresar su contraseña</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-        }
-    });
-    $("#button-login").click(function () { 
-        if($("#username").val() != "" && $("#password").val() != ""){
-            loguearse();
-        }
-    });
-    function loguearse() {
-        var usuario = $("#form-login").serialize();
-        $.ajax({
-            type: "POST",
-            url: "controllers/Usuario.php?peticion=login",
-            data: usuario,
-            success: function (response) {
-                if(response == "correcto"){
-                    location.href ="views/vacaciones/personal";
-                }else{
-                    $("#alert-login").html('<div class="alert alert-warning alert-dismissible fade show" role="alert"><span>'+ response + '</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-                }
-            }
-        });    
-    }
-    
-});
-
 auth();
